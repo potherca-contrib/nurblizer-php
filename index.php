f23f2ed43f2feca91c4dc4f5fe628947967346b1
Ben Peachey <potherca@gmail.com>
2016-04-13 18:57:10 +0200

    Changes the index file to use new structure.

--- index.php
+++ index.php
@@ -1,21 +1,40 @@
-<?php include '_header.php'; ?>
+<?php
 
-<h1>Nurblizer</h1>
-<form action="/nurble.php" method="post">
-    <fieldset>
-        <ul>
-            <li>
-                <label>Text to nurblize</label>
-                <textarea name="text"></textarea>
-            </li>
-            <li>
-                <input type="submit" value="Nurblize Away!">
-            </li>
-        </ul>
-    </fieldset>
-</form>
-<p>
-    <a href="http://www.smbc-comics.com/?id=2779">wtf?</a>
-</p>
+require __DIR__ . '/mustache.php';
 
-<?php include '_footer.php';
+if (array_key_exists('text', $_POST)) {
+    require __DIR__ . '/nurble.php';
+
+    $context = [
+        'title' => 'Your Nurbled Text',
+        'content' => nurble($_POST['text']),
+        'footer.link' => '',
+        'footer.title' => '&lt;&lt; Back',
+    ];
+} else {
+    $context = [
+        'title' => 'Nurblizer',
+        'content' => <<<HTML
+        <form action="" method="post">
+            <fieldset>
+                <ul>
+                    <li>
+                        <label>Text to nurblize</label>
+                        <textarea name="text"></textarea>
+                    </li>
+                    <li>
+                        <input type="submit" value="Nurblize Away!">
+                    </li>
+                </ul>
+            </fieldset>
+        </form>
+HTML
+        ,
+        'footer.link' => 'http://www.smbc-comics.com/?id=2779',
+        'footer.title' => 'wtf?',
+    ];
+}
+
+echo mustache(file_get_contents(__DIR__ . '/template.html'), $context);
+
+/*EOF*/
